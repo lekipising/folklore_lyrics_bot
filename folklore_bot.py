7d0beb62df851399ad5a79625abd5a874c09ab53
lekipising <leeplanke@gmail.com>
2020-11-15 08:26:27 +0300

    First Commit

---
+++ folklore_bot.py
@@ -0,0 +1,27 @@
+from folklore_lyrics import master_list
+
+import datetime
+import random
+import schedule
+import time
+
+
+def lyric():
+    print()
+    songs = []
+    for song in master_list:
+        songs.append(song)
+    
+    r_song = random.choice(songs)
+    for k, v in master_list.items():
+        if k == r_song:
+            print(random.choice(v))
+            print()
+            print('Lyrics from ' + k)
+    
+
+schedule.every(10).seconds.do(lyric)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
